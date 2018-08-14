cd5dc148a21c3e476638f6a0012785e08fcb159c
Peter Estes <peternigelestes@gmail.com>
2018-08-14 09:21:50 -0400

    feature all toggle switches functioning, using arrays to hold their data

--- app/src/pages/paintings/filter-paintings.js
+++ app/src/pages/paintings/filter-paintings.js
@@ -5,59 +5,243 @@ import FormGroup from "@material-ui/core/FormGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import FormHelperText from "@material-ui/core/FormHelperText"
 import Switch from "@material-ui/core/Switch"
+import Button from "@material-ui/core/Button"
+import AddIcon from "@material-ui/icons/Add"
+import { withStyles } from "@material-ui/core/styles"
+
 import { connect } from "react-redux"
-import { FILTERED_PAINTING_PAINTING_SIZE_CHECKED } from "../../constants"
+import { append, reject, contains } from "ramda"
+import {
+  FILTERED_PAINTING_PAINTING_SIZE_CHECKED,
+  FILTERED_PAINTING_PAINTING_SUBJECT_CHECKED,
+  FILTERED_PAINTING_PAINTING_TYPE_CHECKED
+} from "../../constants"
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  }
+})
 
 const FilterPaintings = props => {
-  const { handleSizeChanged, small, medium, large } = props
+  const {
+    classes,
+    withStyles,
+    handleSizeChanged,
+    small,
+    medium,
+    large,
+    handleSubjectChanged,
+    sunsets,
+    cityscapes,
+    wildlife,
+    water,
+    boats,
+    people,
+    nightScenes,
+    dayScenes,
+    handleTypeChanged,
+    original,
+    reproduction
+  } = props
 
-  console.log({ handleSizeChanged, small, medium, large })
+  console.log({
+    handleSizeChanged,
+    handleSubjectChanged,
+    handleTypeChanged
+  })
 
   return (
-    <FormControl component="fieldset">
-      <FormLabel component="legend">Choose your size:</FormLabel>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={small}
-              onChange={handleSizeChanged("18x24")}
-              value="18x24"
+    <React.Fragment>
+      <React.Fragment>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Choose your size:</FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={small}
+                  onChange={handleSizeChanged("18x24")}
+                  value="18x24"
+                />
+              }
+              label="18x24"
             />
-          }
-          label="18x24"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={medium}
-              onChange={handleSizeChanged("24x36")}
-              value="24x36"
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={medium}
+                  onChange={handleSizeChanged("24x36")}
+                  value="24x36"
+                />
+              }
+              label="24x36"
             />
-          }
-          label="24x36"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={large}
-              onChange={handleSizeChanged("32x48")}
-              value="32x48"
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={large}
+                  onChange={handleSizeChanged("32x48")}
+                  value="32x48"
+                />
+              }
+              label="32x48"
             />
-          }
-          label="32x48"
-        />
-      </FormGroup>
-      <FormHelperText>MOOD</FormHelperText>
-    </FormControl>
+          </FormGroup>
+          <FormHelperText>MOOD</FormHelperText>
+        </FormControl>
+      </React.Fragment>
+      {/*/////////////////////////////////////*/}
+      <React.Fragment>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Choose your subject matter:</FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={sunsets}
+                  onChange={handleSubjectChanged("sunsets")}
+                  value="sunsets"
+                />
+              }
+              label="Sunsets"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={cityscapes}
+                  onChange={handleSubjectChanged("cityscapes")}
+                  value="cityscapes"
+                />
+              }
+              label="Cityscapes"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={wildlife}
+                  onChange={handleSubjectChanged("wildlife")}
+                  value="wildlife"
+                />
+              }
+              label="Wildlife"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={water}
+                  onChange={handleSubjectChanged("water")}
+                  value="water"
+                />
+              }
+              label="Water"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={boats}
+                  onChange={handleSubjectChanged("boats")}
+                  value="boats"
+                />
+              }
+              label="Boats"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={people}
+                  onChange={handleSubjectChanged("people")}
+                  value="people"
+                />
+              }
+              label="People"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={nightScenes}
+                  onChange={handleSubjectChanged("nightScenes")}
+                  value="nightScenes"
+                />
+              }
+              label="Night Scenes"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={dayScenes}
+                  onChange={handleSubjectChanged("dayScenes")}
+                  value="dayScenes"
+                />
+              }
+              label="Day Scenes"
+            />
+          </FormGroup>
+          <FormHelperText>MORE MOOD</FormHelperText>
+        </FormControl>
+      </React.Fragment>
+      {/*/////////////////////////////////////*/}
+
+      <React.Fragment>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Choose your type:</FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={original}
+                  onChange={handleTypeChanged("original")}
+                  value="original"
+                />
+              }
+              label="Original"
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={reproduction}
+                  onChange={handleTypeChanged("reproduction")}
+                  value="reproduction"
+                />
+              }
+              label="Reproduction"
+            />
+          </FormGroup>
+          <FormHelperText>EVEN MORE MOOD!</FormHelperText>
+        </FormControl>
+      </React.Fragment>
+      <Button
+        variant="contained"
+        size="large"
+        color="primary"
+        className={classes.button}
+      >
+        Submit
+      </Button>
+    </React.Fragment>
   )
 }
 
 const mapStateToProps = state => {
   return {
-    small: state.filterOptions.size === "18x24" ? true : false,
-    medium: state.filterOptions.size === "24x36" ? true : false,
-    large: state.filterOptions.size === "32x48" ? true : false
+    small: contains("18x24", state.filterOptions.size),
+    medium: contains("24x36", state.filterOptions.size),
+    large: contains("32x48", state.filterOptions.size),
+    size: state.filterOptions.size,
+    sunsets: contains("sunsets", state.filterOptions.subject),
+    cityscapes: contains("cityscapes", state.filterOptions.subject),
+    wildlife: contains("wildlife", state.filterOptions.subject),
+    water: contains("water", state.filterOptions.subject),
+    boats: contains("boats", state.filterOptions.subject),
+    people: contains("people", state.filterOptions.subject),
+    nightScenes: contains("nightScenes", state.filterOptions.subject),
+    dayscenes: contains("cityscapes", state.filterOptions.subject),
+    subject: state.filterOptions.subject,
+    original: contains("original", state.filterOptions.paintingType),
+    reproduction: contains("reproduction", state.filterOptions.paintingType),
+    paintingType: state.filterOptions.paintingType
   }
 }
 
@@ -66,6 +250,18 @@ const mapActionsToProps = dispatch => {
     handleSizeChanged: size => event => {
       dispatch({ type: FILTERED_PAINTING_PAINTING_SIZE_CHECKED, payload: size })
       //this.setState({ [size]: event.target.checked })
+    },
+    handleSubjectChanged: subject => event => {
+      dispatch({
+        type: FILTERED_PAINTING_PAINTING_SUBJECT_CHECKED,
+        payload: subject
+      })
+    },
+    handleTypeChanged: paintingType => event => {
+      dispatch({
+        type: FILTERED_PAINTING_PAINTING_TYPE_CHECKED,
+        payload: paintingType
+      })
     }
   }
 }
@@ -75,4 +271,4 @@ const connector = connect(
   mapActionsToProps
 )
 
-export default connector(FilterPaintings)
+export default connector(withStyles(styles)(FilterPaintings))
--- app/src/reducers/filter-paintings.js
+++ app/src/reducers/filter-paintings.js
@@ -1,16 +1,44 @@
-import { FILTERED_PAINTING_PAINTING_SIZE_CHECKED } from "../constants"
-import { merge } from "ramda"
+import {
+  FILTERED_PAINTING_PAINTING_SIZE_CHECKED,
+  FILTERED_PAINTING_PAINTING_SUBJECT_CHECKED,
+  FILTERED_PAINTING_PAINTING_TYPE_CHECKED
+} from "../constants"
+import { merge, contains, reject, append } from "ramda"
 const initialState = {
-  size: null,
-  subject: null,
-  paintingType: null
+  size: [],
+  subject: [],
+  paintingType: []
 }
 
 export const filterOptions = (state = initialState, action) => {
   switch (action.type) {
     case FILTERED_PAINTING_PAINTING_SIZE_CHECKED:
       //console.log("reducer fired")
-      return merge(state, { size: action.payload })
+      return contains(action.payload, state.size)
+        ? merge(state, {
+            size: reject(aSize => aSize === action.payload, state.size)
+          })
+        : merge(state, { size: append(action.payload, state.size) })
+    case FILTERED_PAINTING_PAINTING_SUBJECT_CHECKED:
+      return contains(action.payload, state.subject)
+        ? merge(state, {
+            subject: reject(
+              aSubject => aSubject === action.payload,
+              state.subject
+            )
+          })
+        : merge(state, { subject: append(action.payload, state.subject) })
+    case FILTERED_PAINTING_PAINTING_TYPE_CHECKED:
+      return contains(action.payload, state.paintingType)
+        ? merge(state, {
+            paintingType: reject(
+              aType => aType === action.payload,
+              state.paintingType
+            )
+          })
+        : merge(state, {
+            paintingType: append(action.payload, state.paintingType)
+          })
     default:
       return state
   }
