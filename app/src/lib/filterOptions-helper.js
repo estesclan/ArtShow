a8810c10e9b18d1605b619e7b8567a35eeb38217
Peter Estes <peternigelestes@gmail.com>
2018-08-16 15:23:18 -0400

    all toggle filters working

--- app/src/lib/filterOptions-helper.js
+++ app/src/lib/filterOptions-helper.js
@@ -12,6 +12,10 @@ export default (paintings, filterOptions) => {
   console.log({ filterOptions })
   console.log({ paintings })
   const filterSizes = () => {
+    if (isEmpty(filterOptions.size)) {
+      //   filterOptions.size = ["18x24", "24x36", "32x48"]
+      return filter(p => true)
+    }
     const predicate18 = contains("18x24", filterOptions.size)
       ? painting => contains("18x24", painting.sizes)
       : always(false)
@@ -22,69 +26,71 @@ export default (paintings, filterOptions) => {
       ? painting => contains("32x48", painting.sizes)
       : always(false)
 
-    if (isEmpty(filterOptions.size)) {
-      filterOptions.size = ["18x24", "24x36", "32x48"]
-    }
     const filteredSizePaintings = filter(
       anyPass([predicate18, predicate24, predicate32])
     )
     return filteredSizePaintings
   }
   ///////////
-  //   const filterSubjects = () => {
-  //     const predicateCity = contains("cityscapes", filterOptions.subject)
-  //       ? painting => contains("cityscapes", painting.subject)
-  //       : always(false)
-  //     const predicateWild = contains("wildlife", filterOptions.subject)
-  //       ? painting => contains("wildlife", painting.subject)
-  //       : always(false)
-  //     const predicateWater = contains("water", filterOptions.subject)
-  //       ? painting => contains("water", painting.subject)
-  //       : always(false)
-  //     const predicateBoats = contains("boats", filterOptions.subject)
-  //       ? painting => contains("boats", painting.subject)
-  //       : always(false)
-  //     const predicatePeople = contains("people", filterOptions.subject)
-  //       ? painting => contains("people", painting.subject)
-  //       : always(false)
-  //     const predicateNight = contains("nightScenes", filterOptions.subject)
-  //       ? painting => contains("night-scenes", painting.subject)
-  //       : always(false)
-  //     const predicateDay = contains("dayScenes", filterOptions.subject)
-  //       ? painting => contains("day-scenes", painting.subject)
-  //       : always(false)
-  //     const predicateSunsets = contains("sunsets", filterOptions.subject)
-  //       ? painting => contains("sunsets", painting.subject)
-  //       : always(false)
+  const filterSubjects = () => {
+    if (isEmpty(filterOptions.subject)) {
+      // filterOptions.paintingType = [
+      //   "cityscapes",
+      //   "wildlife",
+      //   "water",
+      //   "boats",
+      //   "people",
+      //   "nightScenes",
+      //   "dayScenes",
+      //   "sunsets"
+      // ]
+      return filter(p => true)
+    }
+    const predicateCity = contains("cityscapes", filterOptions.subject)
+      ? painting => contains("cityscapes", painting.subject)
+      : always(false)
+    const predicateWild = contains("wildlife", filterOptions.subject)
+      ? painting => contains("wildlife", painting.subject)
+      : always(false)
+    const predicateWater = contains("water", filterOptions.subject)
+      ? painting => contains("water", painting.subject)
+      : always(false)
+    const predicateBoats = contains("boats", filterOptions.subject)
+      ? painting => contains("boats", painting.subject)
+      : always(false)
+    const predicatePeople = contains("people", filterOptions.subject)
+      ? painting => contains("people", painting.subject)
+      : always(false)
+    const predicateNight = contains("nightScenes", filterOptions.subject)
+      ? painting => contains("night-scenes", painting.subject)
+      : always(false)
+    const predicateDay = contains("dayScenes", filterOptions.subject)
+      ? painting => contains("day-scenes", painting.subject)
+      : always(false)
+    const predicateSunsets = contains("sunsets", filterOptions.subject)
+      ? painting => contains("sunsets", painting.subject)
+      : always(false)
 
-  //     if (isEmpty(filterOptions.paintingType)) {
-  //       filterOptions.paintingType = [
-  //         "cityscapes",
-  //         "wildlife",
-  //         "water",
-  //         "boats",
-  //         "people",
-  //         "nightScenes",
-  //         "dayScenes",
-  //         "sunsets"
-  //       ]
-  //     }
-  //     const filteredSubjectPaintings = filter(
-  //       anyPass([
-  //         predicateCity,
-  //         predicateWild,
-  //         predicateWater,
-  //         predicateBoats,
-  //         predicatePeople,
-  //         predicateNight,
-  //         predicateDay,
-  //         predicateSunsets
-  //       ])
-  //     )
-  //     return filteredSubjectPaintings
-  //   }
+    const filteredSubjectPaintings = filter(
+      anyPass([
+        predicateCity,
+        predicateWild,
+        predicateWater,
+        predicateBoats,
+        predicatePeople,
+        predicateNight,
+        predicateDay,
+        predicateSunsets
+      ])
+    )
+    return filteredSubjectPaintings
+  }
   ///////////
   const filterTypes = () => {
+    if (isEmpty(filterOptions.paintingType)) {
+      //   filterOptions.paintingType = ["original", "reproduction"]
+      return filter(p => true)
+    }
     const predicateOrig = contains("original", filterOptions.paintingType)
       ? painting => contains("original", painting.paintingType)
       : always(false)
@@ -92,9 +98,6 @@ export default (paintings, filterOptions) => {
       ? painting => contains("reproduction", painting.paintingType)
       : always(false)
 
-    if (isEmpty(filterOptions.paintingType)) {
-      filterOptions.paintingType = ["original", "reproduction"]
-    }
     const filteredTypePaintings = filter(
       anyPass([predicateOrig, predicateRepro])
     )
@@ -104,14 +107,15 @@ export default (paintings, filterOptions) => {
   console.log(
     "compose",
     compose(
-      //filterSubjects(),
       filterTypes(),
-      filterSizes()
+      filterSubjects()
+
+      //filterSizes()
     )(paintings)
   )
   return compose(
-    //filterSubjects(),
     filterTypes(),
-    filterSizes()
+    filterSizes(),
+    filterSubjects()
   )(paintings)
 }
